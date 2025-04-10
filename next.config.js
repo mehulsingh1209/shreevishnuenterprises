0f2043b0f3bb0d44612874b6ca3c639b6d4b84c9
Mehul Singh <143124065+mehulsingh1209@users.noreply.github.com>
2025-04-10 14:25:20 +0545

    Update and rename Features.js to next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['via.placeholder.com'],
+  },
+  i18n: {
+    locales: ['en', 'hi', 'ne'],
+    defaultLocale: 'en',
+  },
+}
